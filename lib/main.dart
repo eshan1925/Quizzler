77f700a58b85f9e7516ff0f12887251428b6d865
eshan1925 <eshan1925@gmail.com>
2021-04-14 23:34:33 +0530

    Final commit

--- lib/main.dart
+++ lib/main.dart
@@ -109,6 +109,7 @@ class _QuizPageState extends State<QuizPage> {
         Expanded(
           child: Padding(
             padding: EdgeInsets.all(15.0),
+            // ignore: deprecated_member_use
             child: FlatButton(
               textColor: Colors.white,
               color: Colors.green,
@@ -129,6 +130,7 @@ class _QuizPageState extends State<QuizPage> {
         Expanded(
           child: Padding(
             padding: EdgeInsets.all(15.0),
+            // ignore: deprecated_member_use
             child: FlatButton(
               color: Colors.red,
               child: Text(
